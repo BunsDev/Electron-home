01823095c9192ddd709f6d8afb60b39c6b88ca26
kelcheone <kelcheone@gmail.com>
2021-10-17 23:39:16 +0300

    bundling

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { createBrowserHistory, createHashHistory } from "history";
+import { isElectron } from "./utils";
 import Home from "./Home";
 import About from "./About";
-import "./index";
+import "./index.css";
+
+export const history = isElectron()
+  ? createHashHistory()
+  : createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
